cf611fe7b0997ecec7addb47750200479fa38eff
elena g <elena.gartshtein@gmail.com>
2017-10-24 08:30:26 -0400

    experiment with jquery, npm and browserify

---
+++ Codeplayer/Readme.md
@@ -0,0 +1,16 @@
+# A webpage to evaluate HTML CSS JS (jsbin like)
+
+## uses jQuery (managed by npm and browserify)
+
+## install 
+
+$ npm install
+
+## build
+
+$ npm run build
+
+## run
+
+index.html
+
---
+++ Codeplayer/index.js
@@ -0,0 +1,70 @@
+global.$ = require('jquery');
+
+function prepOutput() {
+
+    $('#output').contents().find('head').append('<style></style>');
+    $('#output').contents().find('body').append('<div></div><script><\/script>');
+}
+
+function testData()  {
+    var htm = '<p>lorem<p>';
+    var css = '* {color:green}';
+    var js = 'var el = document.createElement("i"); el.innerHTML="iii"; document.body.appendChild(el)'
+    $('#html textarea').val(htm);
+    $('#css textarea').val(css);
+    $('#js textarea').val(js);
+    setHtml(htm);
+    setCSS(css);
+    setJS(js);   
+}
+
+function setHtml(val) {
+    $('#output').contents().find('div').html(val);
+}
+
+function setCSS(val) {
+    $('#output').contents().find('style').html(val);
+}
+
+/* 
+    in order to dynamically add javascript, I can't simply replace source in the <script> tag.
+    Instead, I can either rebuild script node or run eval() function inside iframe contentWindow.
+    Opted for the first solution
+*/
+
+function setJS(val) {
+    $('#output').contents().find('script').remove();
+    $('#output').contents().find('body').append('<script>' + val + '</\script>');
+}
+
+$(function(){
+    $("#nav li a").click(function(e) {
+        e.preventDefault();
+        var target_panel = this.href.split('#')[1];
+        $("#player").toggleClass(target_panel);
+        var visiblePanels = $('.panel:visible').length;
+        if (visiblePanels) {
+            $('.panel').width(100/visiblePanels  + "%");
+        }
+    });
+
+    
+    $(".panel textarea").on('keyup change', function(){
+          var val = $(this).val();
+          var target_id = $(this).parent().attr("id");
+          var el;
+          switch (target_id) {
+              case 'html':
+                setHtml(val);
+                break;
+              case 'css':
+                setCSS(val);
+                break;
+              case 'js':
+                setJS(val);
+          }
+    });
+
+    prepOutput();
+    testData();
+});
